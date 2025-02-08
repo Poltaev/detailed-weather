21c152f47a6a65da08286e39f297c9bc8ca9f37c
Evgeny Poltaev <evgeny.poltaev@yandex.ru>
2025-02-08 22:57:19 +0400

    update name package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.weatheromparison"
+        applicationId = "com.example.weatherInDetail"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
